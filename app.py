bb6d28a59e9fed56df923c5a8e8f9fc54f63add1
SlayerArman <cartoontv123360@gmail.com>
2026-07-24 07:45:47 +0500

    Main Application Window

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import tkinter as tk
 
 def main ():
     root = tk.Tk()
+
+    root.title("Survey Scout")
+    root.geometry("700x500")
+    root.minsize(500, 350)
+
     root.mainloop()
 
 if __name__ == "__main__":
